85c1aca1befa83b30fed5cea156e4940b6eb110f
QtImM <534368008@qq.com>
2026-03-17 18:03:47 +0800

    "为回测服务提炼独立降级策略接缝"

--- trend-trading-backtest-service/src/main/java/bupt/client/IndexDataClientFeignHystrix.java
+++ trend-trading-backtest-service/src/main/java/bupt/client/IndexDataClientFeignHystrix.java
@@ -1,18 +1,20 @@
 package bupt.client;
 
 import bupt.pojo.IndexData;
-import cn.hutool.core.collection.CollectionUtil;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class IndexDataClientFeignHystrix implements IndexDataClient {
+    private final IndexDataFallbackGateway indexDataFallbackGateway;
+
+    public IndexDataClientFeignHystrix(IndexDataFallbackGateway indexDataFallbackGateway) {
+        this.indexDataFallbackGateway = indexDataFallbackGateway;
+    }
+
     @Override
     public List<IndexData> getIndexData(String code) {
-        IndexData indexData = new IndexData();
-        indexData.setClosePoint(0);
-        indexData.setDate("0000-00-00");
-        return CollectionUtil.toList(indexData);
+        return indexDataFallbackGateway.getIndexData(code);
     }
 }
